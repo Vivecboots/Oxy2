b4cf2f00ecccf4c4085c48a989d7ede640d16eac
Vivecboots <Blairwin05@gmail.com>
2023-05-28 21:51:10 -0700

    mytoken added to list with demo icon.  Add fx

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,14 @@
 import { StaticImageData } from "next/image";
 import EthIcon from "./img/etherLogo.png";
 import UsdcIcon from "./img/usd-coin-usdc-logo.png";
+import MyTokenIcon from "./img/mytoken-logo.jpeg"; // make sure to replace "./img/mytoken-logo.png" with the actual path to your token's icon
+
 export type TokenOptions = {
   label: string;
   icon: StaticImageData;
   value?: string;
 };
+
 export const tokenOptions: TokenOptions[] = [
   { value: undefined, label: "ETH", icon: EthIcon },
   {
@@ -13,9 +16,12 @@ export const tokenOptions: TokenOptions[] = [
     label: "USDC",
     icon: UsdcIcon,
   },
+  {
+    value: "0xYourTokenContractAddress", // replace with your token's contract address
+    label: "MYTOKEN",
+    icon: MyTokenIcon,
+  },
 ];
-
-
 
 
 
