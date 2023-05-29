00fc6404c1ff2b981276e7c12ba5749e75827cb3
Vivecboots <Blairwin05@gmail.com>
2023-05-28 21:59:11 -0700

    Mytoken is functional but now we need to make it send a value

--- utils/index.ts
+++ utils/index.ts
@@ -7,19 +7,22 @@ export type TokenOptions = {
   label: string;
   icon: StaticImageData;
   value?: string;
+  decimals: number;
 };
 
 export const tokenOptions: TokenOptions[] = [
-  { value: undefined, label: "ETH", icon: EthIcon },
+  { value: undefined, label: "ETH", icon: EthIcon, decimals: 18 },
   {
     value: "0x07865c6E87B9F70255377e024ace6630C1Eaa37F", //Görli USDC Smart Contracts address
     label: "USDC",
     icon: UsdcIcon,
+    decimals: 6,
   },
   {
-    value: "0xYourTokenContractAddress", // replace with your token's contract address
+    value: "0xbBc9161Dbf83992953dAAD477646A00b040E6f1A", // replace with your token's contract address
     label: "MYTOKEN",
     icon: MyTokenIcon,
+    decimals: 18, // replace with your token's decimal precision
   },
 ];
 
